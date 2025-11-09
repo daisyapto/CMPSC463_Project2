67e19633a68845a3ebeefc4b6ce58bc3c40fda87
AndrewHerman28 <148911660+AndrewHerman28@users.noreply.github.com>
2025-11-09 09:51:08 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import os
-
-if os.path.exists("/Users/andrewherman/CMP463/Project 2/Data/evacuation_graph.pkl"):
-    with open("/Users/andrewherman/CMP463/Project 2/Data/evacuation_graph.pkl", "rb") as f:
-        G = pickle.load(f)
-
-if G:
-    print(type(G))
