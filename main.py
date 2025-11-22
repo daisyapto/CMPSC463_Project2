ce96d746fc5c6aca45c46673fdf794e6d7899aa2
Daisy <daisyap2005@gmail.com>
2025-11-22 17:03:03 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import random
 from Visualization.mapFoliium import draw_route
 import pickle
 
-roads = gpd.read_file("/Users/andrewherman/CMP463/SafetyGPS/Data/Processed Data/Street_Centerline.geojson")
-floods = gpd.read_file("/Users/andrewherman/CMP463/SafetyGPS/Data/Processed Data/Floods.geojson")
-hurricanes = gpd.read_file("/Users/andrewherman/CMP463/SafetyGPS/Data/Processed Data/Hurricanes.geojson")
-evac_centers = gpd.read_file("/Users/andrewherman/CMP463/SafetyGPS/Data/Processed Data/Facilities.geojson")
+# Add file paths here
+roads = gpd.read_file("")
+floods = gpd.read_file("")
+hurricanes = gpd.read_file("")
+evac_centers = gpd.read_file("")
 
 roads = roads.to_crs(epsg=4326)
 floods = floods.to_crs(epsg=4326)
@@ -19,26 +20,32 @@ flood_union = unary_union(floods.geometry)
 hurricane_union = unary_union(hurricanes.geometry)
 
 G = Graph()
-with open("Data/hazard_graph.pkl", "rb") as f:
+with open("hazard_graph.pkl", "rb") as f:
     loaded_graph = pickle.load(f)
 
 if loaded_graph:
     G.graph = loaded_graph
 
-
-user_pos = Point(39.95, -75.16)  # lat, lon = y,x
+lat = float(input("Enter your latitude coordinate: "))
+lon = float(input("Enter your longitude coordinate: "))
+# user_pos = Point(39.95, -75.16)  # lat, lon = y,x
+user_pos = Point(lat, lon)  # lat, lon = y,x
 distances, previous = G.dijkstra(user_pos)
-print(distances)
 
-# temp_all_paths = []
-# for center in evac.geometry:
-#     goal = (center.x, center.y)
-#     path = G.get_path(previous, goal)
-#     print(path)
-#     temp_all_paths.append(path)
+"""
+temp_all_paths = []
+for center in evac.geometry:
+    goal = (center.x, center.y)
+    path = G.get_path(previous, goal)
+    print(path)
+    temp_all_paths.append(path)
+"""
 
+random_evac = random.choice(evac.geometry)
+path = G.get_path(previous, (random_evac.x, random_evac.y))
+draw_route(user_pos, random_evac, path)
 
-#
-# random_evac = random.choice(evac.geometry)
-# path = G.get_path(previous, (random_evac.x, random_evac.y))
-# draw_route(user_pos, random_evac, path)
+##### Uncomment the following lines to test Prim's ######
+
+test = (-75.21306878970346, 40.02152944119314)
+print(G.primMST(evac.geometry, test))
