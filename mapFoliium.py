b8a48d488aae4ce56e7b1e3d52f250b436138a47
AndrewHerman28 <148911660+AndrewHerman28@users.noreply.github.com>
2025-11-22 12:58:13 -0500

    Add files via upload

---
+++ mapFoliium.py
@@ -0,0 +1,66 @@
+import folium
+import geopandas as gpd
+from folium import GeoJson
+
+
+def draw_route(user_node, evac_node, path):
+    # Read in data
+    roads = gpd.read_file("/Users/andrewherman/CMP463/SafetyGPS/Data/Processed Data/Street_Centerline.geojson")
+    floods = gpd.read_file("/Users/andrewherman/CMP463/SafetyGPS/Data/Processed Data/Floods.geojson")
+    evac = gpd.read_file("/Users/andrewherman/CMP463/SafetyGPS/Data/Processed Data/Facilities.geojson")
+
+    # Convert to EPSG:4326
+    roads = roads.to_crs(epsg=4326)[["geometry"]]
+    floods = floods.to_crs(epsg=4326)[["geometry"]]
+    evac = evac.to_crs(epsg=4326)[["geometry"]]
+
+    # Find center of map
+    center_lat = roads.geometry.bounds[['miny', 'maxy']].mean().mean()
+    center_lon = roads.geometry.bounds[['minx', 'maxx']].mean().mean()
+
+    m = folium.Map(location=[center_lat, center_lon], zoom_start=12)
+
+    # Draw Roads
+    GeoJson(
+        roads,
+        name="Roads",
+        style_function=lambda x: {"color": "blue", "weight": 2, "opacity": 0.7},
+    ).add_to(m)
+
+    # Draw Floods
+    GeoJson(
+        floods,
+        name="Flood Zones",
+        style_function=lambda x: {
+            "fillColor": "red",
+            "color": "red",
+            "weight": 1,
+            "fillOpacity": 0.3,
+        },
+    ).add_to(m)
+
+    # Draw User Location
+    folium.Marker(
+        location=[user_node.x, user_node.y],
+        popup="User Start Location",
+        icon=folium.Icon(color="blue", icon="user"),
+    ).add_to(m)
+
+    # Draw route polyline
+    folium.PolyLine(
+        locations=[(lat, lon) for (lon, lat) in path],
+        weight=5,
+        opacity=0.8,
+        color="green",
+    ).add_to(m)
+
+    # Add marker for that evac center
+    folium.Marker(
+        location=[evac_node.y, evac_node.x],
+        popup="Evacuation Center",
+        icon=folium.Icon(color="green", icon="info-sign"),
+    ).add_to(m)
+    # popup=f"Evac Center<br>Distance = {distances:.2f}",
+
+    folium.LayerControl().add_to(m)
+    m.save("evac_map2.html")
