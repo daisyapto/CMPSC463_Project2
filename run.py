3a453cf8b026795f0facdc891c97ca0f054e7ba8
AndrewHerman28 <148911660+AndrewHerman28@users.noreply.github.com>
2025-11-29 14:24:57 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
 from App.route import app
 
+
+"""
+References:
+https://www.geeksforgeeks.org/python/flask-creating-first-simple-application/
+https://flask.palletsprojects.com/en/stable/quickstart/#a-minimal-application
+https://www.w3schools.com/html/html_forms.asp
+https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit
+Chat GPT-- Flask explanation
+"""
+
 if __name__ == "__main__":
     app.run(debug=True)
