f3e835f6ac7ff8a1047fadd68779395a5df9cb88
Daisy <daisyap2005@gmail.com>
2025-11-29 10:13:52 -0500

    Updated main.py with Flask interface
    - "templates" folder must keep name as templates for Flask
    - "templates" folder must include full_map.html and input.html (program will start with input.html for user location and reroutes to full_map.html)
    - Current bug: input.html does not properly reroute to full_map.html in Flask web display after user location has been input

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
-import random, pickle
+import pickle
 from shapely.geometry import Point
 from Visualization.Full_Map import draw_full_map
 from Algorithms.K_Nearest import k_nearest_evacuation_centers
 from Algorithms.Prims import prim_mst
+from flask import Flask, render_template, request, redirect, url_for
 
-with open("Data/Pickle/hazard_graph.pkl", "rb") as f:
-    graph = pickle.load(f)
+app = Flask(__name__)
+@app.route("/", methods=["GET", "POST"])
 
-with open("Data/Pickle/evac_kdtree.pkl", "rb") as f:
-    tree, evacuation_coords, snapped_centers = pickle.load(f)
+def main():
+    with open("", "rb") as f: # Load hazard_graph.pkl with path
+        graph = pickle.load(f)
 
-center_city = (-75.1575, 39.9509)
-source = Point(center_city[0], center_city[1])
+    with open("", "rb") as f: # Load evac_kdtree.pkl with path
+        tree, evacuation_coords, snapped_centers = pickle.load(f)
 
-# Random source
-# random_lat = random.uniform(39.95, 40.04)
-# random_lon = random.uniform(-75.05, -75.00)
-# source = Point(random_lon, random_lat)
+    #center_city = (-75.1575, 39.9509)
+    if request.method == "POST":
+        longitude = float(request.form.get("longitude"))
+        latitude = float(request.form.get("latitude"))
+        source = Point(longitude, latitude)
+
+        # Random source
+        # random_lat = random.uniform(39.95, 40.04)
+        # random_lon = random.uniform(-75.05, -75.00)
+        # source = Point(random_lon, random_lat)
 
 
-# K nearerst evacuation centers
-K = 8
-routes = k_nearest_evacuation_centers(graph, source, snapped_centers, K, tree)
+        # K nearerst evacuation centers
+        K = 8
+        routes = k_nearest_evacuation_centers(graph, source, snapped_centers, K, tree)
 
-# Get MST edges
-mst_edges = prim_mst(evacuation_coords)
+        # Get MST edges
+        mst_edges = prim_mst(evacuation_coords)
 
-draw_full_map(source, routes, mst_edges, save_file="ui/full_map_merge_test3.html")
+        print("Drawing full map...")
+        draw_full_map(source, routes, mst_edges, save_file="") # full_map.html with path
+        return redirect(url_for(render_template("full_map.html", longitude=longitude, latitude=latitude)))
+
+    return render_template("input.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
