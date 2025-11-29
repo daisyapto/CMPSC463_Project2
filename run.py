20aea51b8e0ef0761bffca551f03e72f38b4ca53
AndrewHerman28 <148911660+AndrewHerman28@users.noreply.github.com>
2025-11-29 16:16:19 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-from App.route import app
-
-
 """
 References:
 https://www.geeksforgeeks.org/python/flask-creating-first-simple-application/
@@ -10,5 +7,19 @@ https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit
 Chat GPT-- Flask explanation
 """
 
+from App.route import app
+import webbrowser, os
+from threading import Timer
+
+
+# Automatically open browser
+def open_browser():
+    webbrowser.open("http://127.0.0.1:5000")
+
+
 if __name__ == "__main__":
+    # Prevent opening browser in debug mode
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        Timer(1, open_browser).start()
     app.run(debug=True)
+
